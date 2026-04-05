f5d15b7701b6f6ee58072eda8b288fef32e029a5
Denys <70490262+user4032@users.noreply.github.com>
2026-04-05 23:44:08 +0300

    Fix syntax error in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Lumyn',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,,
+        scaffoldBackgroundColor: Colors.black,
         splashColor: Colors.transparent,
         highlightColor: Colors.transparent,
         colorScheme: const ColorScheme.dark(
